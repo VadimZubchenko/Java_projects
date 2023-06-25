a5a70250cfd2c34d47a83be957a3108e32d0708b
Vadim Zubchenko <vadimzubchenko@hotmail.com>
2023-06-25 13:34:46 +0300

    added comments into suunnittelumallit factorymethod

--- Java_koulu_proj/Suunnittelutmallit/Suunnittelumallit/src/factorymethod/AterioivaOtus.java
+++ Java_koulu_proj/Suunnittelutmallit/Suunnittelumallit/src/factorymethod/AterioivaOtus.java
@@ -5,28 +5,33 @@ public abstract class AterioivaOtus {
     Juoma juoma = null;
     Ruoka ruoka = null;
 
-    public abstract  Juoma createJuoma();
-    
-    public abstract Tuote luodaanUusi();
-            
+    // rakennetaan alaluokkien (Ope, Opisk ja vahti..) riippuvuuden (dependency) 
+    // Juoma-rajapinnan alla olevista luokista.     
+    public abstract Juoma createJuoma();
+
     public abstract Ruoka createRuoka();
-    
-    
-    public void aterioi(){
-        syö();
+
+    public abstract Tuote luodaanUusi();
+
+    public void aterioi() {
         juo();
+        syö();
     }
 
-    public void syö(){
-        if (ruoka == null)
+    public void juo() {
+        if (juoma == null) {
+            //AterioivaOtus- alaluokissa luodaan juoman rajapinnan alla oleva olion      
+            juoma = createJuoma();
+        }
+        System.out.println("Aterian jälkeen " + juoma + " tekee terää");
+    }
+
+    public void syö() {
+        if (ruoka == null) {
+            //AterioivaOtus- alaluokissa luodaan juoman rajapinnan alla oleva olion      
             ruoka = createRuoka();
+        }
         System.out.println("Kylläpä " + ruoka + " maistuukin hyvältä");
     }
 
-
-    public void juo(){
-        if (juoma == null)
-            juoma = createJuoma();
-        System.out.println("Aterian jälkeen " + juoma + " tekee terää");
-    }
 }
--- Java_koulu_proj/Suunnittelutmallit/Suunnittelumallit/src/factorymethod/Main.java
+++ Java_koulu_proj/Suunnittelutmallit/Suunnittelumallit/src/factorymethod/Main.java
@@ -1,18 +1,26 @@
 package factorymethod;
 //Use the Factory(AterioivaOtus) to get object of concrete class
+
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Opettaja sanoo: ");
+
+        //luodaan opettaja-olion, mikä luo vesi-, ja pullo-oliot
         AterioivaOtus opettaja = new Opettaja();
+
+        //luodut vesi- ja pullo-oliot käytetään yläluokan AterioivaOtus metodissa aterioi()         
         opettaja.aterioi();
         System.out.println("");
-        
+
         System.out.println("Opiskelija sanoo: ");
+        //create opiskelija-olion, mikä luo CocaCola-, ja Pulla-oliot        
         AterioivaOtus opiskelija = new Opiskelija();
+        //luodut CocaCola- ja pullo-oliot käytetään yläluokan AterioivaOtus metodissa aterioi()                 
         opiskelija.aterioi();
         System.out.println("");
-        
+
         System.out.println("Vahtimestari sanoo: ");
         AterioivaOtus vahtimestari = new Vahtimestari();
         vahtimestari.aterioi();
